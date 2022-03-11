a228d32417f8e4c649e1ca157b8e91dce8770166
nazmul-rion <nazmulrion16@gmail.com>
2022-03-11 15:39:52 +0600

    custom spinner

--- src/Components/Spinner/CustomSpinner.js
+++ src/Components/Spinner/CustomSpinner.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import './CustomSpinner.css'
 const CustomSpinner = () => {
     return (
-        <div>
-            Loading...
+        <div className='d-flex justify-content-center p-3'>
+            <div class="loader">
+                <div class="face">
+                    <div class="circle"></div>
+                </div>
+                <div class="face">
+                    <div class="circle"></div>
+                </div>
+            </div>
         </div>
     )
 }
